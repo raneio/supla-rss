8612e9270191a5b701c1487b626b7759860a5a70
Rane <raneahonen@gmail.com>
2019-09-30 20:21:15 +0300

    Use series id instead of search term

--- functions/feed-builder.js
+++ functions/feed-builder.js
@@ -3,36 +3,45 @@ const xmlToJson = require("fast-xml-parser");
 const Podcast = require("podcast");
 
 exports.handler = async (event, context) => {
-  const querySeries = event.queryStringParameters.series
+  const querySeriesId = event.queryStringParameters.series
     ? event.queryStringParameters.series
     : event.path.split("/")[2];
-  const queryFilter = event.queryStringParameters.filter
+
+  const queryFilter = event.queryStringParameters.series
     ? event.queryStringParameters.filter
     : event.path.split("/")[3];
 
-  if (!querySeries || querySeries.length < 4) {
+  const rawSeries = await fetch(
+    `https://prod-component-api.nm-services.nelonenmedia.fi/api/series/${querySeriesId}?app=supla&client=web&userroles=anonymous`
+  )
+    .then(response => response.json())
+    .catch(error => console.error(error));
+
+  if (!rawSeries || rawSeries.error) {
     return {
       statusCode: 404,
-      body: `Series name "${querySeries}" need to be at least 4 character" (404)`
+      body: `Series ${querySeriesId} not found.`
     };
   }
 
-  const searchResult = await fetch(
-    `https://prod-component-api.nm-services.nelonenmedia.fi/api/component/338?offset=0&limit=20&search_term=${querySeries}&app=supla&client=web`
-  ).then(response => response.json());
-
-  if (!searchResult || searchResult.error || searchResult.items.length === 0) {
-    return {
-      statusCode: 404,
-      body: `Not found series (404)`
-    };
-  }
-
-  const series = searchResult.items[0];
+  const series = {
+    title: !queryFilter
+      ? rawSeries.metadata.title
+      : `${rawSeries.metadata.title} (${queryFilter})`,
+    description: rawSeries.metadata.meta.find(
+      meta => meta.name === "description"
+    ).content,
+    imageUrl: rawSeries.metadata.jsonld.thumbnailUrl,
+    siteUrl: rawSeries.metadata.jsonld["@id"]
+  };
 
   const rawEpisodes = await fetch(
-    `https://prod-component-api.nm-services.nelonenmedia.fi/api/component/2600350?offset=0&limit=20&current_primary_content=podcast&current_series_content_order_direction=desc&current_series_id=${series.id}&app=supla&client=web`
-  ).then(response => response.json());
+    `https://prod-component-api.nm-services.nelonenmedia.fi/api/component/2600350?offset=0&limit=${
+      !queryFilter ? 20 : 200
+    }&current_primary_content=podcast&current_series_content_order_direction=desc&current_series_id=${querySeriesId}&app=supla&client=web`
+  )
+    .then(response => response.json())
+    .catch(error => console.error(error));
 
   const episodes = await Promise.all(
     rawEpisodes.items
@@ -41,6 +50,7 @@ exports.handler = async (event, context) => {
           !queryFilter ||
           episode.title.toLowerCase().includes(queryFilter.toLowerCase())
       )
+      .slice(0, 20)
       .map(async episode => {
         return await fetch(
           `https://gatling.nelonenmedia.fi/media-xml-cache?id=${episode.id}&v=3`
@@ -60,16 +70,12 @@ exports.handler = async (event, context) => {
               url: response.Playerdata.Clip.PlayBack.Media.StreamURLs.Audio
             },
             itunesImage: episode.media.images["1280x720"]
-          }));
+          }))
+          .catch(error => console.error(error));
       })
   );
 
-  let feed = new Podcast({
-    title: !queryFilter ? series.title : `${series.title} (${queryFilter})`,
-    description: series.description,
-    imageUrl: series.media.images["1280x720"],
-    siteUrl: `https://www.supla.fi${series.link.href}`
-  });
+  let feed = new Podcast(series);
 
   episodes.forEach(episode => {
     feed.addItem(episode);
